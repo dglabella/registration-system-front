b2655155b8893af52fc57cb8e600653dedc77d4d
ptisseragit <41482924+ptisseragit@users.noreply.github.com>
2022-08-29 19:45:37 -0300

    Registro de Usuarios

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 
 const clienteAxios = axios.create({
-    baseURL:process.env.REACT_APP_BACKEND_URL
-    /*withCredentials: false,
+    baseURL:process.env.REACT_APP_BACKEND_URL,
+    withCredentials: false,
     headers: {
       'Access-Control-Allow-Origin' : '*',
       'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-      }    */
+      }    
 });
 
 export default clienteAxios;
